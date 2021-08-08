c31b9db5796dff05aa6aa08f9818e9eb9a51da27
chalkim <jiangyi2012@outlook.com>
2021-08-08 17:17:00 +0800

    feat(sequence-list): add s10.cpp

---
+++ linear-list/sequence-list/pseudocode-example/s10.cpp
@@ -0,0 +1,18 @@
+/*
+ * 2010
+ * */
+
+void reverse(int R[], int m, int n) {
+	if(m > n) return;
+	for(;m < n ; m++, n--) {
+		int tmp = R[m];
+		R[m] = R[n];
+		R[n] = tmp;
+	}
+}
+
+void converse(int R[], int n, int p) {
+	reverse(R, 0, p-1);
+	reverse(R, p, n-1);
+	reverse(R, 0, n-1);
+}
